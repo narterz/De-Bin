4ba6a7b7c457a6580148b7c7ded652dec8c7825e
jayden <jaydenshelton@yahoo.com>
2025-04-25 14:54:27 -0400

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,58 @@
 import { GoFileBinary } from "react-icons/go";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { 
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuList, 
+  NavigationMenuItem,   
+  NavigationMenuTrigger, 
+  navigationMenuTriggerStyle 
+} from "@/components/ui/navigation-menu";
+
+interface NavigationMenuItems {
+  title: string;
+  description: string;
+  isSelected?: boolean;
+}
+
+const helpMenuContent: NavigationMenuItems[] = [
+  {
+    title: "",
+    description: "",
+  },
+  {
+    title: "",
+    description: "",
+  }
+]
+
+const toolsMenuContent: NavigationMenuItems[] = [
+  {
+    title: "",
+    description: "",
+    isSelected: true
+  },
+  {
+    title: "",
+    description: "",
+    isSelected: false
+  },
+  {
+    title: "",
+    description: "",
+    isSelected: false
+  },
+  {
+    title: "",
+    description: "",
+    isSelected: false
+  },
+  {
+    title: "",
+    description: "",
+    isSelected: false
+  },
+]
 
 export default function Header() {
   return (
@@ -8,16 +61,29 @@ export default function Header() {
         <GoFileBinary className="text-6xl" />
         <h1>De-Bin</h1>
       </div>
-      <div className="h-full flex items-center justify-around w-1/4 p-4">
-        <div className="header-item hover-text">
+      <NavigationMenu className="h-full flex items-center justify-around w-1/4 p-4">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger> <h3>Help</h3> </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {helpMenuContent.map((content) => (
+                  <ListItem></ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        {/* <div className="header-item hover-text">
           <h3>Help</h3>
           <MdKeyboardArrowDown size={20}/>
         </div>
         <div className="header-item hover-text">
           <h3>Tools</h3>
           <MdKeyboardArrowDown size={20}/>
-        </div>
-      </div>
+        </div> */}
+      </NavigationMenu>
     </div>
   );
 }
